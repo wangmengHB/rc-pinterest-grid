586f61d49393e689b745358c931cceef4342f914
meng.wm <meng.wm@alibaba-inc.com>
2019-08-09 17:36:52 +0800

    add build command

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import PinterestGrid from './Grid';
 import makeResponsive from './makeResponsive';
 
-
+export {BreakPoint, PinterestGridProps} from './Grid';
 export default makeResponsive(PinterestGrid);
-
-
-
